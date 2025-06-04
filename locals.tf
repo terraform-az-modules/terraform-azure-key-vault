d5b0a081413a359e4addebeed8994468d0b954f5
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-06-04 23:43:50 +0530

    fix: Updated the custom_name with null

--- locals.tf
+++ locals.tf
@@ -3,7 +3,5 @@
 ## Locals declaration for determining the local variables
 ##-----------------------------------------------------------------------------
 locals {
-  name = var.custom_name != "" ? var.custom_name : module.labels.id
-  # valid_rg_name = var.existing_private_dns_zone == null ? var.resource_group_name : var.existing_private_dns_zone_resource_group_name
-  # private_dns_zone_name = var.enable_private_endpoint ? var.existing_private_dns_zone == null ? azurerm_private_dns_zone.dnszone[0].name : var.existing_private_dns_zone : null
+  name = var.custom_name != null ? var.custom_name : module.labels.id
 }
