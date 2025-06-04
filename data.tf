82ade3a0fd74736ebbe44a71741669f6b5985b24
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-06-04 23:40:31 +0530

    chore: removed unused code format

--- data.tf
+++ data.tf
@@ -2,15 +2,3 @@
 ## Data block to query information 
 ##-----------------------------------------------------------------------------
 data "azurerm_client_config" "current_client_config" {}
-
-##----------------------------------------------------------------------------- 
-## Data block to retreive private ip of private endpoint.
-# ##-----------------------------------------------------------------------------
-# data "azurerm_private_endpoint_connection" "private-ip" {
-#   provider = azurerm.main_sub
-#   count    = var.enabled && var.enable_private_endpoint ? 1 : 0
-
-#   name                = azurerm_private_endpoint.pep[0].name
-#   resource_group_name = var.resource_group_name
-#   depends_on          = [azurerm_key_vault.key_vault]
-# }
