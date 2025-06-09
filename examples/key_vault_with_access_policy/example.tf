24ced16c0a15c47b26d5153ba2bbf361584330d4
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-06-09 20:45:51 +0530

    feat: Added role assignments with for user permission and added resource tags

--- examples/key_vault_with_access_policy/example.tf
+++ examples/key_vault_with_access_policy/example.tf
@@ -6,8 +6,8 @@ data "azurerm_client_config" "current_client_config" {}
 
 
 module "resource_group" {
-  source  = "clouddrove/resource-group/azure"
-  version = "1.0.2"
+  source  = "terraform-az-modules/resource-group/azure"
+  version = "1.0.0"
 
   name        = "app"
   environment = "test"
@@ -81,19 +81,30 @@ module "vault" {
   source                    = "../.."
   name                      = "app"
   environment               = "test"
-  label_order               = ["name", "environment", ]
+  label_order               = ["name", "environment", "location"]
   resource_group_name       = module.resource_group.resource_group_name
-  location                  = module.resource_group.resource_group_location
-  reader_objects_ids        = [data.azurerm_client_config.current_client_config.object_id] # for access policy only use reader or admin
+  location                  = module.resource_group.resource_group_location                # for access policy only use reader or admin
   admin_objects_ids         = [data.azurerm_client_config.current_client_config.object_id] # for access policy only use reader or admin
   subnet_id                 = module.subnet.default_subnet_id[0]
-  enable_rbac_authorization = false
+  enable_rbac_authorization = true
   private_dns_zone_ids      = module.private-dns-zone.private_dns_zone_ids.key_vault
   network_acls = {
     bypass         = "AzureServices"
     default_action = "Deny"
     ip_rules       = ["1.2.3.4/32"]
   }
+
+  reader_objects_ids = {
+    "key vault Read User" = {
+      role_definition_name = "Key Vault Reader"
+      principal_id         = data.azurerm_client_config.current_client_config.object_id
+    },
+    "key vault secerts user" = {
+      role_definition_name = "Key Vault Secrets User"
+      principal_id         = data.azurerm_client_config.current_client_config.object_id
+    }
+  }
+  enable_access_policies     = false
   enable_private_endpoint    = true
   diagnostic_setting_enable  = true
   log_analytics_workspace_id = module.log-analytics.workspace_id ## when diagnostic_setting_enable enable,add log analytics workspace id
