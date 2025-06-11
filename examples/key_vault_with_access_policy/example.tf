ff0d576834cd45862396572d2d3ff24de48db3e9
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-06-11 17:00:41 +0530

    fix: Updated with access_policies and fixed the code format

--- examples/key_vault_with_access_policy/example.tf
+++ examples/key_vault_with_access_policy/example.tf
@@ -4,11 +4,9 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current_client_config" {}
 
-
 module "resource_group" {
-  source  = "terraform-az-modules/resource-group/azure"
-  version = "1.0.0"
-
+  source      = "terraform-az-modules/resource-group/azure"
+  version     = "1.0.0"
   name        = "app"
   environment = "test"
   label_order = ["environment", "name", ]
@@ -16,9 +14,8 @@ module "resource_group" {
 }
 
 module "vnet" {
-  source  = "clouddrove/vnet/azure"
-  version = "1.0.4"
-
+  source              = "clouddrove/vnet/azure"
+  version             = "1.0.4"
   name                = "app"
   environment         = "test"
   label_order         = ["name", "environment"]
@@ -28,20 +25,17 @@ module "vnet" {
 }
 
 module "subnet" {
-  source  = "clouddrove/subnet/azure"
-  version = "1.2.1"
-
+  source               = "clouddrove/subnet/azure"
+  version              = "1.2.1"
   name                 = "app"
   environment          = "test"
   label_order          = ["name", "environment"]
   resource_group_name  = module.resource_group.resource_group_name
   location             = module.resource_group.resource_group_location
   virtual_network_name = module.vnet.vnet_name
-
   #subnet
   subnet_names    = ["subnet1", "subnet2"]
   subnet_prefixes = ["10.30.1.0/24", "10.30.2.0/24"]
-
   # route_table
   routes = [
     {
@@ -66,7 +60,7 @@ module "log-analytics" {
 }
 
 module "private-dns-zone" {
-  source              = "git::https://github.com/ravimalvia10/private-dns-zone.git?ref=feat/private-dns-zone"
+  source              = "git::git@github.com:terraform-az-modules/terraform-azure-private-dns.git?ref=feat/beta"
   resource_group_name = module.resource_group.resource_group_name
   private_dns_config = [
     {
@@ -83,39 +77,29 @@ module "vault" {
   environment                   = "test"
   label_order                   = ["name", "environment", "location"]
   resource_group_name           = module.resource_group.resource_group_name
-  location                      = module.resource_group.resource_group_location                # for access policy only use reader or admin
-  admin_objects_ids             = [data.azurerm_client_config.current_client_config.object_id] # for access policy only use reader or admin
+  location                      = module.resource_group.resource_group_location # for access policy only use reader or admin
   subnet_id                     = module.subnet.default_subnet_id[0]
-  enable_rbac_authorization     = true
+  enable_rbac_authorization     = false
   private_dns_zone_ids          = module.private-dns-zone.private_dns_zone_ids.key_vault
   public_network_access_enabled = true
-  network_acls = {
-    bypass         = "AzureServices"
-    default_action = "Deny"
-    ip_rules       = ["0.0.0.0/0"]
-  }
-
-  reader_objects_ids = {
-    "key vault Read User" = {
-      role_definition_name = "Key Vault Reader"
-      principal_id         = data.azurerm_client_config.current_client_config.object_id
+  enable_access_policies        = true
+  access_policies = {
+    "app-server" = {
+      tenant_id               = data.azurerm_client_config.current_client_config.tenant_id,
+      object_id               = data.azurerm_client_config.current_client_config.object_id,
+      key_permissions         = ["Get", "List"]
+      secret_permissions      = ["Get", "List"]
+      certificate_permissions = ["Get", "List"]
+      storage_permissions     = []
     },
-    "key vault secerts user" = {
-      role_definition_name = "Key Vault Secrets User"
-      principal_id         = data.azurerm_client_config.current_client_config.object_id
-    }
+    "admin-server" = {
+      tenant_id               = data.azurerm_client_config.current_client_config.tenant_id,
+      object_id               = data.azurerm_client_config.current_client_config.object_id,
+      key_permissions         = ["Get", "List", "Create", "Delete", "Purge", "Recover", "Backup", "Restore"]
+      secret_permissions      = ["Get", "List", "Set", "Delete", "Purge", "Recover", "Backup"]
+      certificate_permissions = ["Get", "List", "Create", "Delete", "Purge", "Recover"]
+    },
   }
-  secrets = [
-    {
-      name            = "api-key"
-      value           = "1234567890abcdef"
-      content_type    = "text/plain"
-      not_before_date = null
-      expiration_date = null
-    }
-  ]
-  enable_access_policies     = false
-  enable_private_endpoint    = true
   diagnostic_setting_enable  = true
   log_analytics_workspace_id = module.log-analytics.workspace_id ## when diagnostic_setting_enable enable,add log analytics workspace id
 }
